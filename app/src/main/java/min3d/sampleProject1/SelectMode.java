59fbaa3cb17b81b2a1af7fc463412e5350893d90
Leedobin <dlehqls369@naver.com>
2018-02-12 20:53:42 +0900

    모드 intent

---
+++ app/src/main/java/min3d/sampleProject1/SelectMode.java
@@ -0,0 +1,32 @@
+package min3d.sampleProject1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by LeeDoBin on 2018-02-12.
+ */
+
+public class SelectMode extends Activity{
+    Button freemode;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selectmode);
+
+        freemode = (Button)findViewById(R.id.freeMode);
+
+        freemode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ExampleInsideLayout.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
